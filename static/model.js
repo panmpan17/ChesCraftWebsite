f92b6375dfb61f8999dfad6838b038b5aec26dbf
michaelpan628 <panmpan@gmail.com>
2018-07-22 20:10:51 +0800

    Tweak box order

--- static/model.js
+++ static/model.js
@@ -1,15 +1,5 @@
 seasonOne = [
 	{
-		"id": "UCFP5dcztP_2ayfrXFig6-Qg",
-		"name": "MiMe Bro",
-		"playlist": "PLGTjoQ9c_AgZzKOVH4sCwqAlSmFkc4KDY",
-	},
-	{
-		"id": "UCvuz0-GrFYmBe75bQa1yqtA",
-		"name": "哈記",
-		"playlist": "PLy0CYoUAK74jzyop6kdO0ldHyq15PQ18N",
-	},
-	{
 		"id": "UCiEHmW7zRRZIjBsOKZ4s5AA",
 		"name": "殞月",
 		"playlist": "PLcRttHazKlgoeiYGPlqJlB3SSuDg3wdfP",
@@ -20,8 +10,18 @@ seasonOne = [
 		"playlist": "PLGBpn423WXJmxdkwBIrIbHlOXw4H_54wg",
 	},
 	{
+		"id": "UCFP5dcztP_2ayfrXFig6-Qg",
+		"name": "MiMe Bro",
+		"playlist": "PLGTjoQ9c_AgZzKOVH4sCwqAlSmFkc4KDY",
+	},
+	{
 		"name": "Moco",
 		"id": "UCLW-5M0C7ELByxDc0R_buNA",
 		"playlist": "PL5P1Ua2T-n2lmdF3SwpbHbznNQ--n0QaM",
+	},
+	{
+		"id": "UCvuz0-GrFYmBe75bQa1yqtA",
+		"name": "哈記",
+		"playlist": "PLy0CYoUAK74jzyop6kdO0ldHyq15PQ18N",
 	},
 ]
